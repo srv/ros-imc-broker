9d50ac3b731c2e3959dec0515343b45327c99add
Paulo Dias <paulo.sousa.dias@gmail.com>
2017-12-11 23:45:04 +0000

    Added some RWLock helpers.

---
+++ workspace/src/ros_imc_broker/include/ros_imc_broker/Concurrency/RWLock.hpp
@@ -0,0 +1,48 @@
+//*************************************************************************
+// Copyright (C) 2017 FEUP-LSTS - www.lsts.pt                             *
+//*************************************************************************
+// This program is free software; you can redistribute it and/or modify   *
+// it under the terms of the GNU General Public License as published by   *
+// the Free Software Foundation; either version 2 of the License, or (at  *
+// your option) any later version.                                        *
+//                                                                        *
+// This program is distributed in the hope that it will be useful, but    *
+// WITHOUT ANY WARRANTY; without even the implied warranty of             *
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU       *
+// General Public License for more details.                               *
+//                                                                        *
+// You should have received a copy of the GNU General Public License      *
+// along with this program; if not, write to the Free Software            *
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA          *
+// 02110-1301 USA.                                                        *
+//*************************************************************************
+// Author: Paulo Dias                                                     *
+//*************************************************************************
+
+#ifndef ROS_IMC_BROKER_CONCURRENCY_RW_mutex_HPP_INCLUDED_
+#define ROS_IMC_BROKER_CONCURRENCY_RW_mutex_HPP_INCLUDED_
+
+// ISO C++ headers
+#include <cstdlib>
+#include <cstring>
+#include <string>
+#include <iostream>
+
+// Boost headers.
+#include <boost/thread/shared_mutex.hpp>
+
+namespace ros_imc_broker
+{
+  namespace Concurrency
+  {
+    class RWLock
+    {
+    public:
+      typedef boost::shared_mutex Mutex;
+      typedef boost::shared_lock<Mutex> ReadLock;
+      typedef boost::unique_lock<Mutex> WriteLock;
+    };
+  }
+}
+
+#endif
